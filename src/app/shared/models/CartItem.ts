1419a601529f09bf7da5548e6e3f8334aceb1860
Michael Greatorex <magreatorex@gmail.com>
2023-09-14 12:33:34 +0100

    restart

--- src/app/shared/models/CartItem.ts
+++
@@ -1,8 +0,0 @@
-import { productInfo } from "./productinfo";
-
-export class CartItem{
-  constructor(public shirt:productInfo) { }
-  quantity:number = 1;
-  price: number = this.shirt.price;
-  }
-
--- src/app/shared/models/Order.ts
+++
@@ -1,9 +0,0 @@
-import { CartItem } from "./CartItem";
-
-export class Order{
-  id!:number;
-  items!: CartItem[];
-  totalPrice!:number;
-  name!:string;
-  address!:string;
-}
